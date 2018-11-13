e881ecc1ee775bffdef607e29f4c15c6c783eb95
David Kagramanyan <d.kagramanian@gmail.com>
2018-11-13 23:27:56 +0200

    Using a class realisation to print data
    Changed variable names and edited code a bit to make it cleaner

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ using namespace std;
 
 int main() {
     cout << "Roshen Juicer "<<endl;
-    juicer juicerObj1 (120, 6);
-    juicer juicerObj2 ("nice", 120);
-    juicer juicerObj3 (400, "RED", 100, "roshen");
+    juicer juicerObject1 (120, 6);
+    juicer juicerObject2 ("nice", 120);
+    juicer juicerObject3 (400, "RED", 100, "roshen");
     cout << endl;
     cout << "milka Juicer" << endl;
-    juicer juicerObj4 (200, 10);
-    juicer juicerObj5 ("perfect", 1020);
-    juicer juicerObj6 (2500, "blue", 120, "milka");
+    juicer juicerObject4 (200, 10);
+    juicer juicerObject5 ("perfect", 1020);
+    juicer juicerObject6 (2500, "blue", 120, "milka");
     cout << endl;
     cout << "milenium Juicer" <<endl;
-    juicer juicerObj7 (80, 4);
-    juicer juicerObj8 ("medium", 400);
-    juicer juicerObj9 (100, "yellow", 150, "milenium");
+    juicer juicerObject7 (80, 4);
+    juicer juicerObject8 ("medium", 400);
+    juicer juicerObject9 (100, "yellow", 150, "milenium");
     return 0;
 
 }
